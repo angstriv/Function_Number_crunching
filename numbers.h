31dc116812b57f9f58f4d883b57cc558023c39e6
angstriv <139956817+angstriv@users.noreply.github.com>
2023-08-08 22:15:40 +0800

    Add files via upload

--- numbers.h
+++ numbers.h
@@ -4,3 +4,4 @@ int randnum(int minimum, int maximum);
 int probability(int per_cent);
 int oddNumber(int number);//判断奇数
 int evenNumber(int number);//判断偶数
+void intSwap(int* num1_p, int* num2_p);//交换2个整型数字
