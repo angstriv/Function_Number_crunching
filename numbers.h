e9192b239702c312f5a716df53c12530849c5f0c
angstriv <139956817+angstriv@users.noreply.github.com>
2023-08-10 22:41:53 +0800

    Add files via upload

--- numbers.h
+++ numbers.h
@@ -1,7 +1,8 @@
 #pragma once
 
-int randnum(int minimum, int maximum);
-int probability(int per_cent);
+int randnum(int minimum, int maximum);//随机值(最小值,最大值)
+int probability(int per_cent);//概率(百分数)
 int oddNumber(int number);//判断奇数
 int evenNumber(int number);//判断偶数
+int primeNumber(int number);//判断质数
 void intSwap(int* num1_p, int* num2_p);//交换2个整型数字
